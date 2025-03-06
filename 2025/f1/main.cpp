bfadaeb0ff50afd64552f2ac25e9267a6055d824
espoirbaraka <esbarakabigega@gmail.com>
2025-03-06 09:42:16 +0100

    tab1

---
+++ 2025/f1/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+float calculatrice(float nbre1, float nbre2, char signe){
+    if(signe == '+'){
+        return nbre1+nbre2;
+    } else if (signe == '-'){
+        return nbre1-nbre2;
+    } else if (signe == '*'){
+        return nbre1*nbre2;
+    } else if (signe == '/'){
+        if(nbre2 == 0){
+            return 0;
+        } else {
+            return nbre1/nbre2;
+        }
+
+    } else {
+        return nbre1+nbre2;
+    }
+}
+
+int main()
+{
+    cout<<calculatrice(12, 2, '(');
+}
